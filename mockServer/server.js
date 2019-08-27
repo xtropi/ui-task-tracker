31d01d65c769d025040042d5e3c8fda043779476
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2019-08-27 18:17:32 +0300

    update npm packs

--- mockServer/server.js
+++ mockServer/server.js
@@ -52,7 +52,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // access to public folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 
 
